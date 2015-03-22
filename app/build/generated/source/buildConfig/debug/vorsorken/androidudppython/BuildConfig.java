48b360b10a7fbdfe2b7568a9821f26a38dc6df97
Fer Contreras <fercreek@gmail.com>
2015-03-22 11:37:38 -0600

    Files

---
+++ app/build/generated/source/buildConfig/debug/vorsorken/androidudppython/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vorsorken.androidudppython;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vorsorken.androidudppython";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
